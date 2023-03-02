52971895ae589908f7959f96bc71b6d8e7bf27a3
Himansu9888 <123152485+Himansu9888@users.noreply.github.com>
2023-03-02 08:39:25 +0500

    Variables

--- main.js
+++ main.js
@@ -1,123 +1,153 @@
-// Basic Maths in Javascript
-// Types of Numbers
-// Integers e.g. 10, 400, or -5.
-// Floating point numbers(floats) ex:- 12.5, 45.234544;
-// Doubles spedific type of floating
+// const buttonA = document.querySelector("#button_A");
+// const headingA = document.querySelector("#heading_A");
 
-// Number: BigInt64Array, Octal, Hexadecimal;
+// buttonA.onclick = () => {
+//   const name = prompt("what is your name?");
+//   alert(`Hello ${name}, nice to see you!`);
+//   headingA.textContent = `Welcome ${name}`;
+// };
 
-// It's all numbers to me
-// const myInt = 5;
-// const myFloat = 6.622;
-// myInt;
-// myFloat;
-// console.log(typeof myInt);
-// console.log(typeof myFloat);
+// Without a Variable
 
-// Useful Number methods
-// const lotsOfDecimal = 1.75545689389449;
-// lotsOfDecimal;
-// const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
-// twoDecimalPlaces;
+// const buttonB = document.querySelector("#button_B");
+// const headingB = document.querySelector("#heading_B");
 
-// Converting to number data types
-// let myNumber = "45";
-// myNumber += 3;
-// myNumber;
+// buttonB.onclick = () => {
+//   alert(`Hello ${prompt("what is your name?")}, nice to see you!`);
+//   headingB.textContent = `Welcome ${prompt("what is your name?")}`;
+// };
+
+// Declaring a Variable
+// let myName;
+// let myAge;
+// myName;
+// myAge;
+// scoobyDoo;
+
+// Initializing a variable
+// myName = "himansu";
+// myAge = "19";
+// myName;
+// myAge;
+
+// let myDog = "Tommy";
+// myDog;
+
+// A note about var
+// var myName;
+// var myAge;
+
+// var myName = "Himansu";
+
+// function logName() {
+//   console.log(myName);
+// }
+// logName();
+
+// var myName;
+
+// var myName = 'Himansu';
+// var myName = 'rinky';
+// you can declare many times a variable with var
+// but using let not
+// let myName = 'Himansu';
+// let myName = 'rinky';
+
+// you'd have to do this instead
+// let myName = 'Himansu';
+// myName = 'rinky';
+// myName
+
+// Updating a variable
+// myName = 'rinky';
+// myAge = 40;
+
+// As aside on variable naming rules
+// discuss details in MDN
+// Good name examples
+// age
+// myAge
+// init
+// initialColor
+// finalOutputValue
+// audio1
+// audio2
+
+// Bad name examples
+// 1
+// a
+// _12
+// myage
+// MYAGE
+// var
+// Document
+// skjfndskjfndbdskjfb
+// thisisareallylongvariablenameman
+
+// Variable types
+
+// Numbers
+// let myAge = 19;
+// myAge;
+
+// Strings ('' or "")
+// let dolphinGoodbye = 'So long and thanks for all the fish';
+// dolphinGoodbye;
+
+// Booleans
+// let iAmAlive = true;
+// iAmAlive;
+
+// let test = 6 > 3;
+// test;
+
+// Arrays
+// let myNameArray =['Himansu', 'Rinky', 'papuni'];
+// let myNumberArray = [10, 15, 50];
+// myNameArray;
+// myNumberArray;
+//* you can access each:
+// console.log(myNameArray[0]);
+// console.log(myNumberArray[0]);
+
+// Objects
+// let dog = {name : 'Tommy', greed : 'Dalmatian'};
+// dog;
+// console.log(dog.name);
+// console.log(dog.greed);
+// console.log(dog);
+
+// Dynamic Typing
+// let myString = 'Hello';
+// myString;
+
+// let myNumber = '509';
+// oops, this is still a string
 // console.log(typeof myNumber);
-// to fix
-// let myNumber = "45";
-// myNumber = Number(myNumber) + 3;
-// myNumber;
+// myNumber = 509;
+// much better - now this is a number
 // console.log(typeof myNumber);
 
-// Arithmetic Operators
-// console.log(4 + 3);
-// console.log(20 - 12);
-// console.log(3 * 3);
-// console.log(12/4);
-// console.log(8%3);
-// console.log(4**2);
+// Constants in Javascript
+//* You must initialize them when you declare them
+//* you can't assign them a new value after you've initialized them
+//  let count;
+//  const count; // error
 
-// const num1 = 10;
-// const num2 = 50;
-// console.log(9 * num1);
-// console.log(num1 ** 3);
-// console.log(num2 / num1);
+// let count = 1;
+// count;
+// count = 2;
+// count;
 
-// console.log(4 + 3 + 10);
-// console.log(num2 % 9) * num1;
-// console.log(num2 + num1 / 8 + 2);
+// const count = 1;
+// count = 2
 
-// Operator Precedence
-// const num1 = 10;
-// const num2 = 50;
-// console.log(num2 + num1 / 8 + 2)
-//* multiply and divide are always done first then add and subtract(calculation is always evaluated from left to right);
-//* you can put ();
-// console.log((num2 + num1) / (8 + 2));
+// but
+// const bird = {species : 'Kestrel'};
+// console.log(bird.species);
+// you can add,remove, update like this;
+// bird.species = 'Striated Caracara';
+// console.log(bird.species);
 
-// Increment and decrement operators
-// guessCount++;
-// 3++;
-// let num1 = 4;
-// num1++;
-// num1;
-
-// let num2 = 6;
-// num2--;
-// num2;
-
-// Assignment Operators
-// let x = 3; // x contains value of 3
-// let y = 4; // y contains value of 4
-// x = y; // x now contains the same value of y contains, 4
-// x
-// y
-
-//  let x = 3;
-//  let y = 4;
-//  x += 4; // x = x + 4
-//  x
-//  x -= 3; // x = x - 4
-//  x
-// x *= 3; // x = x * 3
-// x
-// x /= 5; // x = x / 5
-// x
-
-// Active learning: sizing a canvas box
-// see in MDN page for understand only math parts
-// let x = 50;
-// let y = 50;
-
-// x = 50;
-// y = 50;
-// ctx.fillStyle = 'green';
-// ctx.fillRect(10, 10, x, y);
-
-// Comparison Operators
-// console.log(5 === 2 + 4);
-// console.log(5 === 2 + 3);
-// console.log(5 !== 2 + 3);
-// console.log(5 !== 2 + 4);
-// console.log(10 < 2);
-// console.log(10 > 2);
-// console.log(10 <= 2);
-// console.log(10 >= 2);
-
-//* Another Active Learning
-const btn = document.querySelector("button");
-const txt = document.querySelector("p");
-
-btn.addEventListener("click", updateBtn);
-
-function updateBtn() {
-  if (btn.textContent === "start machine") {
-    btn.textContent = "stop machine";
-    txt.textContent = "the machine has started!";
-  } else {
-    btn.textContent = "start machine";
-    txt.textContent = "the machine is stopped";
-  }
-}
+// When to use const and when to use let
+//* details in MDN

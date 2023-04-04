37e8820434f41172e6d59a3c0f9e9feb6b9d8783
Himansu9888 <123152485+Himansu9888@users.noreply.github.com>
2023-04-04 05:04:23 +0500

    Add files via upload

---
+++ A-1st-splash-into-javascript/main.js
@@ -0,0 +1,79 @@
+// function checkGuess() {
+//   alert("I am a placeholder");
+// }
+// checkGuess();
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(10 * 10);
+console.log(10 / 2);
+
+const name = "Bingo";
+name;
+const hello = " says hello!";
+hello;
+const greeting = name + hello;
+greeting;
+
+let name1 = "Bingo";
+name1 += " says hello!";
+
+//  or
+
+let name2 = "Bingo";
+name2 = name2 + " says hello!";
+
+5 === 2 + 4; // false
+"Chris" === "Bob"; // false
+5 === 2 + 3; // true
+2 === "2"; // false; number versus string
+console.log(5 === 2 + 4);
+console.log("Chris" === "Bob");
+console.log(5 === 2 + 3);
+console.log(2 === "2");
+
+5 !== 2 + 4; // true
+"Chris" !== "Bob"; // true
+5 !== 2 + 3; // false
+2 !== "2"; // true; number versus string
+console.log(5 !== 2 + 4);
+console.log("Chris" !== "Bob");
+console.log(5 !== 2 + 3);
+console.log(2 !== "2");
+
+6 < 10; // true
+20 < 10; // false
+console.log(6 < 10);
+console.log(20 < 10);
+
+6 > 10; // false
+20 > 10; // true
+console.log(6 > 10);
+console.log(20 > 10);
+
+const fruits = ["apples", "bananas", "cherries"];
+for (const fruit of fruits) {
+  console.log(fruit);
+}
+
+const $name = ["car", "bike", "cycle", "train", "aeroplane"];
+
+for (const car of $name) {
+  console.log(car);
+}
+
+// but
+
+const $car = ["maruti", "suzuki", "bmw", "hyundai"];
+for (let i = 0; i < $car.length; i++) {
+  console.log($car[i]);
+}
+
+const $flower = ["rose", "haragaura", "gendu", "tagar"];
+for (let i = 0; i < $flower.length; i++) {
+  console.log($flower[i]);
+}
+const _machine = ["dozer", "jcb", "bullet", "soldiertruck"];
+for (let i = 0; i < _machine.length; i++) {
+  console.log(_machine[i] + " " + " himansu");
+}
